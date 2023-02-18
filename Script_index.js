3dc3d36a3209e10fea1675c6e3738f17ad111d43
Adrian Bourdelande <adrianbourdelande@gmail.com>
2023-02-18 20:14:11 +0100

    Mejorar el About
    Mejorar el About
    Crear CSS about
    Crear JS para index

---
+++ Script_index.js
@@ -0,0 +1,11 @@
+"use strict"
+window.addEventListener("load", function(){
+    var miniatura_01 = document.querySelector("#miniatura_01");
+    var titulo_01 = document.createElement("p");
+    var galeria_lotus = document.querySelector("#galeria_lotus");
+    
+    galeria_lotus.onmouseover = function(){
+        edicion_01_original.src="/pictures/WEB_retouching_IMG_0346_edited.jpg";
+    };
+
+})
